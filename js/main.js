3dc6c1c70fd4d9b183246e9150e6136dade4c863
SergioFiloth <58532720+SergioFiloth@users.noreply.github.com>
2020-12-04 22:00:28 -0500

    First See Web Site
    Up files for see in github pages

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* let ubicacionPrincipal  = window.pageYOffset;
+window.onscroll = () => {
+    let Desplazamiento_Actual = window.pageYOffset,
+        navbar = document.getElementById("nav")
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        navbar.style.top = '0';
+    }
+    else{
+        navbar.style.top = '-100px';
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+} */
+
+// let menuT = document.getElementById("menuT")
