6278ced93b770806f8728ed6f61e02e557f3a4c3
SergioFiloth <58532720+SergioFiloth@users.noreply.github.com>
2020-12-05 21:27:38 -0500

    Add function in faqs
    This function in the spanish your title is "Arcordeon"

--- js/main.js
+++ js/main.js
@@ -12,3 +12,26 @@ window.onscroll = () => {
 } */
 
 // let menuT = document.getElementById("menuT")
+
+let faqsT = () => {
+    let btnItems = document.querySelectorAll(".btnItem")
+    if (btnItems) {
+        for (let i = 0; i < btnItems.length; i++){
+            btnItems[i].addEventListener("click", e => {
+                let et = e.target
+                if (et.className == "btnItem active") {
+                    removeClass();
+                } else {
+                    removeClass();
+                    btnItems[i].classList.add("active")
+                }
+            })
+        }
+        let removeClass = () => {
+            for (let i = 0; i < btnItems.length; i++){
+                btnItems[i].classList.remove("active")
+            }
+        }
+    }
+}
+faqsT();
