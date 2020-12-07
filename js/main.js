9c9604606a60dfde533b37058c290c91383ad540
SergioFiloth <58532720+SergioFiloth@users.noreply.github.com>
2020-12-07 08:30:44 -0500

    Updates in the proyect
    The update include footer and more of the website and dinamic menu

--- js/main.js
+++ js/main.js
@@ -1,37 +1,50 @@
-/* let ubicacionPrincipal  = window.pageYOffset;
+// Hide Navbar
+
+let principalUbication = window.pageYOffset,
+    navbar = document.getElementById("nav"),
+    hNavbar = navbar.offsetHeight,
+    fean = document.getElementById("fean")
 window.onscroll = () => {
-    let Desplazamiento_Actual = window.pageYOffset,
-        navbar = document.getElementById("nav")
-    if(ubicacionPrincipal >= Desplazamiento_Actual){
+    let actualMove = window.pageYOffset
+    if(principalUbication >= actualMove){
         navbar.style.top = '0';
-    }
-    else{
+    } else{
         navbar.style.top = '-100px';
     }
-    ubicacionPrincipal = Desplazamiento_Actual;
-} */
+    principalUbication = actualMove;
+}
 
-// let menuT = document.getElementById("menuT")
+console.log(hNavbar)
 
-let faqsT = () => {
-    let btnItems = document.querySelectorAll(".btnItem")
-    if (btnItems) {
-        for (let i = 0; i < btnItems.length; i++){
-            btnItems[i].addEventListener("click", e => {
-                let et = e.target
-                if (et.className == "btnItem active") {
-                    removeClass();
-                } else {
-                    removeClass();
-                    btnItems[i].classList.add("active")
-                }
-            })
-        }
-        let removeClass = () => {
-            for (let i = 0; i < btnItems.length; i++){
-                btnItems[i].classList.remove("active")
+fean.style.marginTop = `calc(${hNavbar}px + .6rem)`
+
+// FAQs Card
+
+let btnItems = document.querySelectorAll(".btnItem")
+if (btnItems) {
+    for (let i = 0; i < btnItems.length; i++){
+        btnItems[i].addEventListener("click", e => {
+            let et = e.target
+            if (et.className == "btnItem active") {
+                removeClass();
+            } else {
+                removeClass();
+                btnItems[i].classList.add("active")
             }
+        })
+    }
+    let removeClass = () => {
+        for (let i = 0; i < btnItems.length; i++){
+            btnItems[i].classList.remove("active")
         }
     }
 }
-faqsT();
+
+// Procces
+
+let linkM = document.getElementById("linkM")
+let link = linkM.dataset.link
+
+linkM.addEventListener("click", () => {
+    console.log("Here you go to marketplace")
+})
